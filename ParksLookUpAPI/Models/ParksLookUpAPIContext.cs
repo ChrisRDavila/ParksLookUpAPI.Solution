68bc5e53bc499017aeff5a7b12fd9d5cb42ce51d
ChrisRDavila <crossdavila@gmail.com>
2023-06-09 09:36:29 -0700

    project bare bones restart with correct project name

--- ParksLookUpAPI/Models/ParksLookUpAPIContext.cs
+++ ParksLookUpAPI/Models/ParksLookUpAPIContext.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ProjectName.Models
 {
-  public class ProjectNameContext : DbContext
+  public class ParksLookUpAPIContext : DbContext
   {
     public DbSet<ClassName> ClassName { get; set; }
     public DbSet<ClassName2> ClassName2 { get; set; }
