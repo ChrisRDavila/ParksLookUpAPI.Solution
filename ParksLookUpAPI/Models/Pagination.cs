e7df4873f30b9b6f0ade6700a7b4b4ecf47b214d
ChrisRDavila <crossdavila@gmail.com>
2023-06-09 11:34:28 -0700

    All CRUD functionaluty works for swagger atm, possibly at mvp, adding class for pagination

--- ParksLookUpAPI/Models/Pagination.cs
+++ ParksLookUpAPI/Models/Pagination.cs
@@ -2,6 +2,9 @@ namespace ParksLookUpAPI.Models
 {
   public class Pagination
   {
-    
+    public List<Park> Parks { get; set; }
+    public int PageItems { get; set; }
+    public int CurrentPage { get; set; }
+    public int PageSize { get; set; }
   }
 }
