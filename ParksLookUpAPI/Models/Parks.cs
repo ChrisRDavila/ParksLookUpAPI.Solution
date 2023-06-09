c6eeb559b6955ebc9c9e6a0759d373a2d7ebcd54
ChrisRDavila <crossdavila@gmail.com>
2023-06-09 10:39:04 -0700

    add required to properies before running migration and updating database

--- ParksLookUpAPI/Models/Parks.cs
+++ ParksLookUpAPI/Models/Parks.cs
@@ -7,9 +7,13 @@ namespace ParksLookUpAPI.Models
   public class Park
   {
     public int ParkId { get; set; }
+    [Required(ErrorMessage = "The parks's name can't be empty!")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "The park's state can't be empty!")]
     public string State { get; set; }
+    [Required(ErrorMessage = "The park's features can't be empty!")]
     public string Features { get; set; }
+    [Range(0, 10, ErrorMessage = "Rating must be between 0 and 10.")]
     public int Rating { get; set; }
 
   }
